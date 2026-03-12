5b36aaa84d389af4f25ca58d7601b54ecbb53133
muhammadridho-debug <idomuhammad11@gmail.com>
2026-03-12 17:04:06 +0800

    mengimplementasikan logika pergeseran didalam insertion sort

--- InsertionSortAlgo.cpp
+++ InsertionSortAlgo.cpp
@@ -26,7 +26,12 @@ void insertionsort() {
     int j;
 
     for (int i = 1; i < n; i++) {
-        
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
     }
 }
 
