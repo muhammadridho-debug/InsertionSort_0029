611b0406e05a1b7b2e2cc8dcd916bb7c7760ce6f
muhammadridho-debug <idomuhammad11@gmail.com>
2026-03-12 17:00:08 +0800

    memasukkan fungsi input dengan validasi ukuran array

--- InsertionSortAlgo.cpp
+++ InsertionSortAlgo.cpp
@@ -4,7 +4,24 @@ using namespace std;
 int arr[20];
 int n;
 
+void input() {
+    while (true) {
+        cout << "Masukan Jumlah Data Pada Array : ";
+        cin >> n;
+        if (n <= 20) break;
+        else cout << "\nArray Tidak Boleh Lebih Dari 20\n";
+    }
+    cout << "\n=============================================";
+    cout << "\n==========Masukan Elemen Array==============";
+    cout << "\n=============================================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
+
 int main()
 {
-    
+
 }
